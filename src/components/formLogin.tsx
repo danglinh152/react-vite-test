74df7b67b28393e3a8c91727304fb3e292485177
ThanhLamUser <vanlamhv5@gmail.com>
2025-02-23 17:42:20 +0700

    loginform

--- src/components/formLogin.tsx
+++ src/components/formLogin.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import type { FormProps } from "antd";
-import { Button, Checkbox, Form, Input } from "antd";
+import { Button, Checkbox, Form, Input, Tabs } from "antd";
 import Loader from "./loader";
+import {
+  LoginOutlined,
+  UserAddOutlined,
+} from "@ant-design/icons";
 
 type FieldType = {
   username: string;
@@ -41,6 +45,105 @@ const onFinishFailed: FormProps<FieldType>["onFinishFailed"] = (errorInfo) => {
   console.log("Failed:", errorInfo);
 };
 
+const LoginChildren = () => {
+  return (
+    <Form
+    name="basic"
+    labelCol={{ span: 8 }}
+    wrapperCol={{ span: 16 }}
+    style={{ maxWidth: 600,marginRight:24,minWidth:400 }}
+    initialValues={{ remember: true }}
+    onFinish={onFinish}
+    onFinishFailed={onFinishFailed}
+    autoComplete="off"
+
+  >
+    <Form.Item<FieldType>
+      label="Username"
+      name="username"
+      rules={[{ required: true, message: "Please input your username!" }]}
+    >
+      <Input />
+    </Form.Item>
+
+    <Form.Item<FieldType>
+      label="Password"
+      name="password"
+      rules={[{ required: true, message: "Please input your password!" }]}
+      style={{ marginBottom: 12 }}
+    >
+      <Input.Password />
+    <a style={{ float: 'right',marginTop: 12 }} href="">
+            Forgot password
+    </a>
+    </Form.Item>
+
+    {/* <Form.Item<FieldType> name="remember" valuePropName="checked" label={null}>
+      <Checkbox>Remember me</Checkbox>
+    </Form.Item> */}
+
+    <Form.Item label={null}>
+      <Button type="primary" htmlType="submit">
+        Submit
+      </Button>
+    </Form.Item>
+  </Form>
+  )
+}
+
+const RegisterChildren = () => {
+  return (
+    <Form 
+    name="basic"
+    labelCol={{ span: 8 }}
+    wrapperCol={{ span: 16 }}
+    style={{ maxWidth: 600,marginRight:24,minWidth:400 }}
+    initialValues={{ remember: true }}
+    onFinish={onFinish}
+    onFinishFailed={onFinishFailed}
+    autoComplete="off"
+
+  >
+    <Form.Item<FieldType>
+      label="Username"
+      name="username"
+      rules={[{ required: true, message: "Please input your username!" }]}
+    >
+      <Input />
+    </Form.Item>
+
+    <Form.Item<FieldType>
+      label="Username"
+      name="username"
+      rules={[{ required: true, message: "Please input your username!" }]}
+    >
+      <Input />
+    </Form.Item>
+
+    <Form.Item<FieldType>
+      label="Password"
+      name="password"
+      rules={[{ required: true, message: "Please input your password!" }]}
+    >
+      <Input.Password />
+    </Form.Item>
+
+    {/* <Form.Item<FieldType> name="remember" valuePropName="checked" label={null}>
+      <Checkbox>Remember me</Checkbox>
+    </Form.Item> */}
+
+    <Form.Item label={null}>
+      <Button type="primary" htmlType="submit">
+        Submit
+      </Button>
+    </Form.Item>
+  </Form>
+  )
+}
+
+
+
+
 const FormLogin: React.FC = () => (
   <>
   <div className="container" style={{
@@ -48,46 +151,28 @@ const FormLogin: React.FC = () => (
     justifyContent: "center",  
     alignItems: "center",     
     height: "100vh",     
-    background: "#f0f2f5"      
+    // background: "#f0f2f5"      
   }}
   >
-    <Form
-      name="basic"
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 16 }}
-      style={{ maxWidth: 600,marginRight:24 }}
-      initialValues={{ remember: true }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
+    <Tabs centered
+    defaultActiveKey="1"
+    items={[
+      {
+        key: "1",
+        label: "Login",
+        children:<LoginChildren />, // Nội dung của Tab 1
+        icon: <LoginOutlined />,
+      },
+      
+      {
+        key: "2",
+        label: "Register",
+        children: <RegisterChildren />, // Nội dung của Tab 2
+        icon: <UserAddOutlined />,
+      },
+    ]}
+  />
 
-    >
-      <Form.Item<FieldType>
-        label="Username"
-        name="username"
-        rules={[{ required: true, message: "Please input your username!" }]}
-      >
-        <Input />
-      </Form.Item>
-  
-      <Form.Item<FieldType>
-        label="Password"
-        name="password"
-        rules={[{ required: true, message: "Please input your password!" }]}
-      >
-        <Input.Password />
-      </Form.Item>
-  
-      {/* <Form.Item<FieldType> name="remember" valuePropName="checked" label={null}>
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item> */}
-  
-      <Form.Item label={null}>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
   <Loader/>
   </div>
   </>
