2e94ec581543d95490381d7d6720bf7d7b4cf9ed
ThanhLamUser <vanlamhv5@gmail.com>
2025-03-05 22:43:36 +0700

    nexttoslider

--- src/components/slider/NextoSlider.tsx
+++ src/components/slider/NextoSlider.tsx
@@ -1,13 +1,9 @@
 
 export default function NextoSlider() {
     return(
-        <div style={{ width:"35%",height:"100%" }}>
-        <a href="">
-          <img style={{ width:"100%",height:156,marginBottom:20 }} src="http://localhost:8080/storage/upload/MCBooks_KCt3_840x320.png" alt="" />
-        </a>
-        <a href="">
-          <img style={{width:"100%",height:156 }} src="http://localhost:8080/storage/upload/MCBooks_KCt3_840x320.png" alt="" />
-        </a>
+        <div style={{ width:"35%",height:"100%",display:"flex",flexDirection:"column",justifyContent:"space-between" }}>
+          <img style={{ width:"100%",height:200,marginBottom:20 }} src="http://localhost:8080/storage/upload/MCBooks_KCt3_840x320.png" alt="" />
+          <img style={{width:"100%",height:200 }} src="http://localhost:8080/storage/upload/MCBooks_KCt3_840x320.png" alt="" />
       </div>
     )
 }
